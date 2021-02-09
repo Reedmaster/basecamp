e0782f6f1b873554b67fd789e339bd4eefb07136
Alex Reed <areed22496@gmail.com>
2021-02-09 15:23:28 +0000

    added validation for projects

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -29,4 +29,14 @@ class ProjectsTest extends TestCase
 
         $this->get('/projects')->assertSee($attributes['title']);
     }
+
+    public function test_a_project_requires_a_title()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('title');
+    }
 }

b016ece19ab17e43894300ac2042165d7c1ac6db
Alex Reed <areed22496@gmail.com>
2021-02-26 16:00:42 +0000

    added form partials and style changes

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,44 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="POST" 
-        action="/projects" 
-        class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow"
-    >
-        
-        @csrf
-
+    <div class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow">
         <h1 class="text-2xl font-normal mb-10 text-center">
             Create a project
         </h1>
 
-        <div class="field mb-6">
-            <label class="label text-sm mb-2 block" for="title">Title</label>
-
-            <div class="control">
-                <input type="text" 
-                    class="input bg-transparent border border-gray-500 rounded p-2 text-xs w-full" 
-                    name="title" 
-                    placeholder="Title">
-            </div>
-        </div>
-
-        <div class="field">
-            <label class="label" for="description">Description</label>
-
-            <div class="control">
-                <textarea name="description" 
-                    id="" 
-                    class="textarea bg-transparent border border-gray-500 rounded p-2 text-xs w-full"
-                ></textarea>
-            </div>
-        </div>
-
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="btn-red">Create Project</button>
-                <a href="/projects">Cancel</a>
-            </div>
-        </div>
-    </form>
+        <form method="POST" 
+            action="/projects" 
+        >
+            {{-- This passes in a new instance of project, so no information is need to pass in like edit.blade.php --}}
+            @include('projects._form',[
+                'project' => new App\Models\Project,
+                'buttonText' => 'Create Project',
+            ])
+        </form>
+    </div>
 @endsection
